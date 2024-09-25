c2b7008229b8dc6e249f0d93709c296546e30893
Кирилл Т <hash0xcb8c@yandex.ru>
2024-09-25 23:48:10 +0300

    Add created_at DateTimeField to blog.models

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -98,7 +98,11 @@ class Category(BaseModel):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     text = models.TextField(verbose_name='текст комментария')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
