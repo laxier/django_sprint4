89eabce8a8f65a7743c6f5e8c133688c48e68892
Кирилл Т <hash0xcb8c@yandex.ru>
2024-09-26 00:41:02 +0300

    Make migrations

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
+from django.conf.urls.static import static
+
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 
@@ -31,4 +34,4 @@ urlpatterns = [
          ),
          name='registration',
          ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
