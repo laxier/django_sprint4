7742a0820544536e38d817b095212cb914463601
Кирилл Т <hash0xcb8c@yandex.ru>
2024-09-26 00:47:28 +0300

    W291 trailing whitespace

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -34,4 +34,4 @@ urlpatterns = [
          ),
          name='registration',
          ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
