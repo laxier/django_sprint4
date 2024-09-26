735996b1b80a7f2281027177e0b73f18b8ad715b
Кирилл Т <hash0xcb8c@yandex.ru>
2024-09-26 16:27:14 +0300

    Added custom_500_view, page_not_found views

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-
+from django.shortcuts import render
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
@@ -7,3 +7,15 @@ class AboutPageView(TemplateView):
 
 class RulesPageView(TemplateView):
     template_name = 'pages/rules.html'
+
+
+def custom_500_view(request, *args, **kwargs):
+    return render(request, 'pages/500.html', status=500)
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
